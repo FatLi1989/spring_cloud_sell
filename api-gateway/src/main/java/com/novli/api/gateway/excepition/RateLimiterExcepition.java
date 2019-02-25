9a4220c4d78aa3e834e3a24abeffe5a0464dec6f
FatLi1989 <514340686@qq.com>
2019-02-25 09:36:58 +0800

    today

---
+++ api-gateway/src/main/java/com/novli/api/gateway/excepition/RateLimiterExcepition.java
@@ -0,0 +1,7 @@
+package com.novli.api.gateway.excepition;
+
+public class RateLimiterExcepition extends RuntimeException {
+
+    private String code;
+
+}
---
+++ api-gateway/src/main/java/com/novli/api/gateway/fillter/ResponseHeaderFilter.java
@@ -0,0 +1,37 @@
+package com.novli.api.gateway.fillter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+@Component
+public class ResponseHeaderFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+        response.setHeader("x-Foo", UUID.randomUUID().toString());
+        return null;
+    }
+}
